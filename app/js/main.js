9d507ea2e71d28798d8afcdd006e3bde8bef6eed
dspmnt <mak2207@mail.ru>
2015-10-02 02:37:24 +0700

    На сдачу Первой части

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,47 @@
-$(function() {
-console.log('Файл main.js успешно загружен')
-});
+/*Реализуем стилизацию и добавление картинки*/
+$(function(){
+    var wrapper = $( ".file_upload" ),
+        inp = wrapper.find( "input" ),
+        btn = wrapper.find( ".button" ),
+        lbl = wrapper.find( "mark" );
+
+    var file_api = ( window.File && window.FileReader && window.FileList && window.Blob ) ? true : false;
+
+    inp.change(function(){
+        var file_name;
+        if( file_api && inp[ 0 ].files[ 0 ] )
+            file_name = inp[ 0 ].files[ 0 ].name;
+        else
+            file_name = inp.val().replace( "C:\\fakepath\\", '' );
+
+        if( ! file_name.length )
+            return;
+
+        if( lbl.is( ":visible" ) ){
+            lbl.text( file_name );
+            btn.text( "Выбрать" );
+        }else
+            btn.text( file_name );
+    }).change();
+
+});
+$( window ).resize(function(){
+    $( ".file_upload input" ).triggerHandler( "change" );
+});
+/*Всплывающее POPUP окно Добавления проекта*/
+ ;(function($) {
+    $(function() {
+        var popup;
+        $('#popup-button').on('click', function(e){
+            e.preventDefault();
+     popup = $('#pop-it-up').bPopup({
+            speed: 650,
+            transition: 'slideDown',
+            transitionClose: 'slideDown',
+            modalColor:'#58697a'
+        });
+        });
+
+    });
+ })(jQuery);
+ /*Закрытие окна Добавления проекта*/
