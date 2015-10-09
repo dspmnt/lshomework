cb523f42dc52d1e40492d2ba7ceb3e8c3fc4258b
dspmnt <mak2207@mail.ru>
2015-10-09 23:59:36 +0700

    Исправил косяки в ИЕ8

---
+++ app/js/contacts.js
@@ -0,0 +1,7 @@
+ /*----------IE8 Placeholder-----------*/
+ $(document).ready(function(){
+    //      if(!Modernizr.input.placeholder){
+            $('input, textarea').placeholder();
+        //  }
+        
+ });
